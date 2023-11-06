d5d89d29ee1629d9d47cb4d1e70683311975fb1e
Kaleemullah Bhutto <94223501+BhuttoKaleem@users.noreply.github.com>
2023-11-06 13:31:34 +0500

    Js assignmts done

---
+++ W2D5/jsAssignments/arrayMax.js
@@ -0,0 +1,4 @@
+array = [2,1,9,3,5,8,1,6,9,7,0];
+
+
+console.log(Math.max(...array));
---
+++ W2D5/jsAssignments/functionTable.js
@@ -0,0 +1,8 @@
+function table(num){
+    let i =1;
+    while (i<=10) {
+        console.log(i+"*"+num+"="+num*i);
+        i++;
+    }
+}
+table(15);
