902cfb623569f8769b6acdec0768e5a734fa0df4
Kaleemullah Bhutto <94223501+BhuttoKaleem@users.noreply.github.com>
2023-12-05 12:53:46 +0500

    working with custom errors

--- W6D5/controllers/todo_controller.js
+++ W6D5/controllers/todo_controller.js
@@ -1,28 +1,32 @@
 const todo = require('../models/todo_model');
+const CustomError = require("../middleware/CustomError");
+const catchAsyncError = require("../middleware/catchAsyncError");
 
-async function getTasks(req,res){
+const getTasks = catchAsyncError(async (req,res)=>{
+    // itis = jkdlsdj;
+    // throw CustomError("not found",404);
  const tasks = await todo.find({});
  return res.json({tasks});
-}
+})
 
-async function createTasks(req,res){
+ const createTasks = catchAsyncError(async (req,res)=>{
     const { taskText, taskStatus } = req.body; // json 
     const todoCreated = todo({ taskText: taskText, taskStatus: taskStatus })
     todoCreated.save();
     res.json(
         todoCreated)
-}
+})
 
-async function completedTasks(req,res){
+const completedTasks = catchAsyncError(async (req,res)=>{
     const tasks = await Todo.find({ taskStatus: true })
     res.json({tasks})
-}
+})
 
 
-async function inCompleteTasks(req,res){
+const inCompleteTasks = catchAsyncError(async (req,res)=>{
     const tasks = await Todo.find({ taskStatus: false })
     res.json({tasks})
-}
+})
 
 module.exports = {
     getTasks,createTasks, completedTasks, inCompleteTasks
