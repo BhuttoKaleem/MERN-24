5afbb60f296d9a97e1441531f715ccf9c6b2ba86
Kaleemullah Bhutto <94223501+BhuttoKaleem@users.noreply.github.com>
2023-12-06 13:20:06 +0500

    changes

--- W6D5/controllers/todo_controller.js
+++ W6D5/controllers/todo_controller.js
@@ -2,14 +2,14 @@ const todo = require('../models/todo_model');
 const CustomError = require("../middleware/CustomError");
 const catchAsyncError = require("../middleware/catchAsyncError");
 
-const getTasks = catchAsyncError(async (req,res)=>{
+getTasks = catchAsyncError(async (req,res)=>{
     // itis = jkdlsdj;
     // throw CustomError("not found",404);
  const tasks = await todo.find({});
  return res.json({tasks});
 })
 
- const createTasks = catchAsyncError(async (req,res)=>{
+createTasks = catchAsyncError(async (req,res)=>{
     const { taskText, taskStatus } = req.body; // json 
     const todoCreated = todo({ taskText: taskText, taskStatus: taskStatus })
     todoCreated.save();
@@ -17,17 +17,42 @@ const getTasks = catchAsyncError(async (req,res)=>{
         todoCreated)
 })
 
-const completedTasks = catchAsyncError(async (req,res)=>{
-    const tasks = await Todo.find({ taskStatus: true })
+completedTasks = catchAsyncError(async (req,res)=>{
+    const tasks = await todo.find({ taskStatus: true })
     res.json({tasks})
 })
 
 
-const inCompleteTasks = catchAsyncError(async (req,res)=>{
-    const tasks = await Todo.find({ taskStatus: false })
+inCompleteTasks = catchAsyncError(async (req,res)=>{
+    const tasks = await todo.find({ taskStatus: false })
     res.json({tasks})
+})
+
+UpdateTasks = catchAsyncError( async(req, res) => {
+    const { id, taskText, taskStatus } = req.body; //JSON BODY
+    const findTask = await todo.findById(id);
+    if (findTask) {
+        findTask.taskText = taskText;
+        findTask.taskStatus = taskStatus;
+        findTask.save();
+        res.json({ success: true, findTask })
+    } else {
+        res.status(404).json({
+            success: false,
+            message: "Task Not found"
+        })
+    }
+})
+
+deleteById = catchAsyncError( async(req, res) => {
+    const { id } = req.params;
+    const deletedTask = await todo.findByIdAndDelete(id); // deletes whose id is given with delete/abc
+    res.json({
+        success: true,
+        deletedTask
+    })
 })
 
 module.exports = {
-    getTasks,createTasks, completedTasks, inCompleteTasks
+    getTasks,createTasks, completedTasks, inCompleteTasks,UpdateTasks,deleteById,
 }
--- W6D5/routes/todo_routes.js
+++ W6D5/routes/todo_routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getTasks,createTasks,completedTasks, inCompleteTasks} = require('../controllers/todo_controller');
+const {getTasks,createTasks,completedTasks, inCompleteTasks,UpdateTasks} = require('../controllers/todo_controller');
 
 //all tasks
 router.post('/', createTasks);
@@ -16,49 +16,27 @@ router.get('/InComplete',inCompleteTasks);
 
 
 router.route("/").get(getTasks).post(createTasks);
-// router.patch('/update', async(req, res) => {
-//     const { id, taskText, taskStatus } = req.body; //JSON BODY
-//     const findTask = await TodoModel.findById(id);
-//     if (findTask) {
-//         findTask.taskText = taskText;
-//         findTask.taskStatus = taskStatus;
-//         findTask.save();
-//         res.json({ success: true, findTask })
-//     } else {
-//         res.status(404).json({
-//             success: false,
-//             message: "Task Not found"
-//         })
-//     }
-
-// })
+router.patch('/update',UpdateTasks)
 
 
 
-// router.delete('/delete', async(req, res) => {
-//     const { id } = req.query;
-//     const deletedData = await TodoModel.findByIdAndDelete(id); // deletes whose id is given with ?id=abc
-//     res.json({
-//         success: true,
-//         deletedData
-//     })
-// })
+router.delete('/delete', async(req, res) => {
+    const { id } = req.query;
+    const deletedData = await TodoModel.findByIdAndDelete(id); // deletes whose id is given with ?id=abc
+    res.json({
+        success: true,
+        deletedData
+    })
+})
 
-// router.delete('/delete/:id', async(req, res) => {
-//     const { id } = req.params;
-//     const deletedData = await TodoModel.findByIdAndDelete(id); // deletes whose id is given with delete/abc
-//     res.json({
-//         success: true,
-//         deletedData
-//     })
-// })
+router.delete('/delete/:id',deleteById)
 
-// router.delete('/delete/', async(req, res) => {
-//     const { id } = req.body;
-//     const deletedData = await TodoModel.findByIdAndDelete(id); // deletes whose id is given 
-//     res.json({
-//         success: true,
-//         deletedData
-//     })
-// })
+router.delete('/delete/', async(req, res) => {
+    const { id } = req.body;
+    const deletedData = await TodoModel.findByIdAndDelete(id); // deletes whose id is given 
+    res.json({
+        success: true,
+        deletedData
+    })
+})
 module.exports = router;
